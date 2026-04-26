1926a9be42e07eaacaa4f583fb83b48e36995479
José Cabrera <joseacabrera@protonmail.com>
2026-04-25 21:47:32 -0300

    feat: create notes widget

---
+++ frontend/src/widgets/NotesWidget.tsx
@@ -0,0 +1,33 @@
+import type { WidgetProps } from '../types/widgets';
+
+type NotesConfig = {
+	text: string;
+};
+
+// TODO: List of notes
+export default function NotesWidget({ config, onConfigChange }: WidgetProps) {
+	const notesConfig = config as NotesConfig;
+
+	const handleChange = (value: string) => {
+		onConfigChange({
+			...notesConfig,
+			text: value,
+		});
+	};
+
+	return (
+		<div style={{ padding: 8, height: '100%' }}>
+			<textarea
+				value={notesConfig?.text ?? ''}
+				onChange={(e) => handleChange(e.target.value)}
+				placeholder='Write your notes...'
+				style={{
+					width: '100%',
+					height: '100%',
+					resize: 'none',
+					outline: 'none',
+				}}
+			/>
+		</div>
+	);
+}
